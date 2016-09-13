00c581ba507be80bd3ecc61a6bc8b3fd48fca526
Jamie Kelly <jamie@studio174.ca>
2016-09-13 10:51:34 -0700

    updates db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+
+    	Model::unguard();
+
+    	// $this->call('UsersTableSeeder');
+    	$this->call(BooksTableSeeder::class);
+
+    	Model::reguard();
+        
     }
 }
