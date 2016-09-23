43fe962daba2f095250fcfa5082b2debc960d400
Jamie Kelly <jamie@studio174.ca>
2016-09-23 11:53:44 -0700

    adds GET show method

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 /**
  * Class BooksController
@@ -17,6 +18,24 @@ class BooksController
   public function index()
   {
     return Book::all();
+  }
+
+/**
+ * GET /books/{id}
+ * @param integer $id
+ * @return mixed
+ */
+  public function show($id)
+  {
+    try {
+      return Book::findOrFail($id);
+    } catch (ModelNotFoundException $e) {
+      return response()->json([
+        'error' => [
+          'message' => 'Book not found',
+        ],
+      ], 404);
+    }
   }
 
 }
