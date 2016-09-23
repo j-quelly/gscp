2d732906507ab084da3367325fec3127217f4abe
Jamie Kelly <jamie@studio174.ca>
2016-09-23 11:55:44 -0700

    adds fixed migration'

--- tests/app/Http/Controllers/BooksControllerTest.php
+++ tests/app/Http/Controllers/BooksControllerTest.php
@@ -35,17 +35,58 @@ class BooksControllerTest extends TestCase
   /** @test **/
   public function show_should_return_a_valid_book()
   {
-    $this->markTestIncomplete('Pending test');
+
+    echo "\n\r{$this->yellow}It should return a valid book...";
+
+    $this
+      ->get('/v1/books/1')
+      ->seeStatusCode(200)
+      ->seeJson([
+        'id'          => 1,
+        'title'       => 'War of the Worlds',
+        'description' => 'A science fiction masterpiece about Martians invading London',
+        'author'      => 'H. G. Wells',
+      ]);
+
+    $data = json_decode($this->response->getContent(), true);
+    $this->assertArrayHasKey('created_at', $data);
+    $this->assertArrayHasKey('updated_at', $data);
+
+    echo " {$this->green}[OK]{$this->white}\n\r";
+  }
+
+/** @test **/
+  public function show_should_fail_when_the_book_id_does_not_exist()
+  {
+    
+    echo "\n\r{$this->yellow}It should fail when the book id does not exist...";
+
+    $this
+      ->get('/v1/books/99999')
+      ->seeStatusCode(404)
+      ->seeJson([
+        'error' => [
+          'message' => 'Book not found',
+        ],
+      ]);
+
+    echo " {$this->green}[OK]{$this->white}\n\r";
   }
 
   /** @test **/
-  public function show_should_fail_when_the_book_id_does_not_exist()
-  {
-    $this->markTestIncomplete('Pending test');
-  }
-
   public function show_route_should_not_match_an_invalid_route()
   {
-    $this->markTestIncomplete('Pending test');
+    echo "\n\r{$this->yellow}It should not match an invalid route...";
+
+    $this->get('/v1/books/this-is-invalid');
+
+    $this->assertNotRegExp(
+      '/Book not found/',
+      $this->response->getContent(),
+      'BooksController@show route matching when it should not.'
+    );
+
+    echo " {$this->green}[OK]{$this->white}\n\r"; 
+
   }
 }
