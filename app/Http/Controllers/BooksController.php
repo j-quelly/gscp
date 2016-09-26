cac2f2ea8dbb7d5ecd6bc662933affb3078fa352
Jamie Kelly <jamie@studio174.ca>
2016-09-26 14:21:47 -0700

    updates store method

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Book;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
 
 /**
  * Class BooksController
@@ -46,7 +47,10 @@ class BooksController
   public function store(Request $request)
   {
     $book = Book::create($request->all());
-    return response()->json(['created' => true], 201);
+
+    return response()->json(['created' => true], 201, [
+      'Location' => route('books.show', ['id' => $book->id]),
+    ]);
   }
 
 }
