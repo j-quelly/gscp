89bf91332cf0d4c356898b28b2381f02ff8b1529
Jamie Kelly <jamie@studio174.ca>
2016-09-26 14:22:44 -0700

    adds new test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,13 +2,47 @@
 
 class TestCase extends Laravel\Lumen\Testing\TestCase
 {
-    /**
-     * Creates the application.
-     *
-     * @return \Laravel\Lumen\Application
-     */
-    public function createApplication()
-    {
-        return require __DIR__.'/../bootstrap/app.php';
-    }
+  /**
+   * Creates the application.
+   *
+   * @return \Laravel\Lumen\Application
+   */
+  public function createApplication()
+  {
+    return require __DIR__ . '/../bootstrap/app.php';
+  }
+
+/**
+ * See if the response has a header.
+ *
+ * @param $header
+ * @return $this
+ */
+  public function seeHasHeader($header)
+  {
+    $this->assertTrue(
+      $this->response->headers->has($header),
+      "Response should have the header '{$header}' but does not."
+    );
+    return $this;
+  }
+
+/**
+ * Asserts that the response header matches a given regular expression
+ *
+ * @param $header
+ * @param $regexp
+ * @return $this
+ */
+  public function seeHeaderWithRegExp($header, $regexp)
+  {
+    $this
+      ->seeHasHeader($header)
+      ->assertRegExp(
+        $regexp,
+        $this->response->headers->get($header)
+      );
+    return $this;
+  }
+  
 }
