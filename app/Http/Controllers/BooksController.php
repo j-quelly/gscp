37ede005e15f75f758d4ec6a18833eb0ef8e9a08
Jamie Kelly <jamie@studio174.ca>
2016-10-06 11:45:56 -0700

    revises books show method

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -28,15 +28,16 @@ class BooksController
  */
   public function show($id)
   {
-    try {
-      return Book::findOrFail($id);
-    } catch (ModelNotFoundException $e) {
-      return response()->json([
-        'error' => [
-          'message' => 'Book not found',
-        ],
-      ], 404);
-    }
+    return Book::findOrFail($id);
+    // try {
+    //   return Book::findOrFail($id);
+    // } catch (ModelNotFoundException $e) {
+    //   return response()->json([
+    //     'error' => [
+    //       'message' => 'Book not found',
+    //     ],
+    //   ], 404);
+    // }
   }
 
 /**
